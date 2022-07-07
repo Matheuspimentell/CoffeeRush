646630c4eeb89a71d44a4f5c9345498455e7aad2
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2022-07-07 20:28:04 -0300

    :game_die: finalizando movimentacao
    :bug: corrigido bug que fazia o player ficar preso nas pontas
    :bug: corrigido bug que fazia o player cair com a cabeca no chao

--- Coffee Rush/Assets/Scripts/PlayerMovement.cs
+++ Coffee Rush/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,20 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller; //Reference to the character controller
     private float h_movement = 0f; //Variable to keep track of the horizontal movement
+    private bool isJumping = false;
     public float speed = 30f; //Speed of the player
     
     void Update()
     {
         //Every frame, get input from the player
         h_movement = Input.GetAxisRaw("Horizontal") * speed;
+        isJumping = Input.GetButtonDown("Jump");
+
     }
 
     private void FixedUpdate()
     {
         //Make the player move if the input is > 0 and < 1 || > -1 and < 0
-        controller.Move(h_movement * Time.fixedDeltaTime, false, false);
+        controller.Move(h_movement * Time.fixedDeltaTime, false, isJumping);
     }
 }
