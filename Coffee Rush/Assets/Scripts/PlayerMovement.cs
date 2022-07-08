32adda8829e85b3db88187c179815d058e43c4a1
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2022-07-07 23:16:46 -0300

    :game_die: colocando o platform effector nas plataformas
    Para que o jogador possa subir nas plataformas ao pular quando estiver debaixo delas

--- Coffee Rush/Assets/Scripts/PlayerMovement.cs
+++ Coffee Rush/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller; //Reference to the character controller
     private float h_movement = 0f; //Variable to keep track of the horizontal movement
-    private bool isJumping = false;
+    private bool isJumping = false; //Variable to keep track if a player is jumping
     public float speed = 30f; //Speed of the player
     
     void Update()
     {
         //Every frame, get input from the player
         h_movement = Input.GetAxisRaw("Horizontal") * speed;
-        isJumping = Input.GetButtonDown("Jump");
 
+        //If the player pressed the Jump Button
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
     }
 
     private void FixedUpdate()
     {
         //Make the player move if the input is > 0 and < 1 || > -1 and < 0
         controller.Move(h_movement * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
     }
 }
